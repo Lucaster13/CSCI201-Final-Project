cc8acfeb2eba0e5cea612dacda7625bc28ecff7c
Martin Romo <romoma@usc.edu>
2018-10-28 15:41:36 -0700

    Update to database files

--- DBHandler.java
+++ DBHandler.java
@@ -114,18 +114,18 @@ public class DBHandler {
 	/*
 	Returns a list of the stored passwords for the user with the given ID
 	*/
-	public static ArrayList<Password> getPasswords(int userID) {
-		ArrayList<Password> results = new ArrayList<Password>();
+	public static ArrayList<DBPassword> getPasswords(int userID) {
+		ArrayList<DBPassword> results = new ArrayList<DBPassword>();
 		if(conn==null) createConnection();
 		if(conn != null) {
 			PreparedStatement ps=null;
 			ResultSet rs=null;
 			try {
-				ps=conn.prepareStatement("SELECT passwordID, app_name, encrypted_pass, last_update, suggested_reset FROM password WHERE userID=?");
+				ps=conn.prepareStatement("SELECT passwordID, username, app_name, encrypted_pass, last_update, suggested_reset FROM password WHERE userID=?");
 				ps.setInt(1, userID);
 				rs=ps.executeQuery();
 				while(rs.next()) {
-					results.add(new Password(rs.getInt("passwordID"), rs.getString("app_name"), rs.getString("encrypted_pass"), rs.getString("last_update"), rs.getString("suggested_reset")));
+					results.add(new Password(rs.getInt("passwordID"), rs.getString("username"), rs.getString("app_name"), rs.getString("encrypted_pass"), rs.getString("last_update"), rs.getString("suggested_reset")));
 				}
 			} catch(SQLException e) {
 				System.out.println(e.getMessage());
@@ -144,8 +144,8 @@ public class DBHandler {
 	/*
 	Returns a list of the security questions for the password with the given ID
 	*/
-	public static ArrayList<SecurityQuestion> getSecurityQuestions(int passwordID) {
-		ArrayList<SecurityQuestion> results = new ArrayList<SecurityQuestion>();
+	public static ArrayList<DBSecurityQuestion> getSecurityQuestions(int passwordID) {
+		ArrayList<DBSecurityQuestion> results = new ArrayList<DBSecurityQuestion>();
 		if(conn==null) createConnection();
 		if(conn != null) {
 			PreparedStatement ps=null;
@@ -170,16 +170,74 @@ public class DBHandler {
 		}
 		return results;
 	}
+	
+	public static boolean addPassword(int userID, String username, String app_name, String encrypted_pass, String last_update, String suggested_reset) {
+		boolean success = false;
+		if(conn==null) createConnection();
+		if(conn != null) {
+			PreparedStatement ps=null;
+			ResultSet rs=null;
+			try {
+				ps=conn.prepareStatement("INSERT INTO password (userID, username, app_name, encrypted_pass, last_update, suggested_reset) VALUES (?,?,?,?,?,?)");
+				ps.setInt(1, userID);
+				ps.setString(2, username);
+				ps.setString(3, app_name);
+				ps.setString(4, encrypted_pass);
+				ps.setString(5, last_update);
+				ps.setString(6, suggested_reset);
+				ps.executeUpdate();
+				success=true;
+			} catch(SQLException e) {
+				System.out.println(e.getMessage());
+			} finally {
+				try {
+					if(rs != null) rs.close();
+					if(ps != null) ps.close();
+				} catch(SQLException e) {
+					System.out.println(e.getMessage());
+				}
+			}
+		}
+		return success;
+	}
+	
+	public static boolean addQuestion(int passwordID, String question, String answer) {
+		boolean success = false;
+		if(conn==null) createConnection();
+		if(conn != null) {
+			PreparedStatement ps=null;
+			ResultSet rs=null;
+			try {
+				ps=conn.prepareStatement("INSERT INTO security_question (passwordID, question, answer) VALUES (?,?,?)");
+				ps.setInt(1, passwordID);
+				ps.setString(2, question);
+				ps.setString(3, answer);
+				ps.executeUpdate();
+				success=true;
+			} catch(SQLException e) {
+				System.out.println(e.getMessage());
+			} finally {
+				try {
+					if(rs != null) rs.close();
+					if(ps != null) ps.close();
+				} catch(SQLException e) {
+					System.out.println(e.getMessage());
+				}
+			}
+		}
+		return success;
+	}
 }
 
-class Password {
+class DBPassword {
 	private int passwordID;
+	private String username;
 	private String app_name;
 	private String encrypted_pass;
 	private Date last_update;
 	private Date suggested_reset;
 	private static SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
-	Password(int passwordID, String app_name, String encrypted_pass, String last_update, String suggested_reset) {
+	DBPassword(int passwordID, String app_name, String encrypted_pass, String last_update, String suggested_reset) {
 		this.passwordID=passwordID;
 		this.app_name=app_name;
 		this.encrypted_pass=encrypted_pass;
@@ -196,6 +254,9 @@ class Password {
 	}
 	int getID() {
 		return passwordID;
+	}
+	String getUsername() {
+		return username;
 	}
 	String getName() {
 		return app_name;
@@ -214,10 +275,10 @@ class Password {
 	}
 }
 
-class SecurityQuestion {
+class DBSecurityQuestion {
 	private String question;
 	private String answer;
-	SecurityQuestion(String question, String answer) {
+	DBSecurityQuestion(String question, String answer) {
 		this.question=question;
 		this.answer=answer;
 	}
--- database.sql
+++ database.sql
@@ -1,6 +1,6 @@
 DROP DATABASE IF EXISTS password_manager;
-CREATE DATABASE password_manager;
-USE password_manager;
+CREATE DATABASE password_protector;
+USE password_protecteor;
 CREATE TABLE user (
 	userID int(10) PRIMARY KEY NOT NULL AUTO_INCREMENT,
 	username varchar(100) NOT NULL UNIQUE,
@@ -12,6 +12,7 @@ CREATE TABLE user (
 CREATE TABLE password (
 	passwordID int(15) PRIMARY KEY NOT NULL AUTO_INCREMENT,
 	userID int(10) NOT NULL,
+    username varchar(100) NOT NULL,
 	app_name varchar(100) NOT NULL,
 	encrypted_pass varchar(250) NOT NULL,
 	last_update datetime NOT NULL,
@@ -25,3 +26,23 @@ CREATE TABLE security_question (
 	answer varchar(150) NOT NULL,
 	FOREIGN KEY fk2(passwordID) REFERENCES password(passwordID)
 );
+INSERT INTO user (username, master_pass, last_update, email, phone) VALUES
+	("user1","$_dasd2131dadar2","2018-10-25","user1@test.com","(555) 555-5555"),
+    ("user2","$_aetwgfsdfarqda","2018-10-27","user2@test.com","(555) 555-5556"),
+    ("user3","$_asdbaiuhg272ca","2018-10-28","user3@test.com","(555) 555-5557");
+INSERT INTO password (userID, username, app_name, encrypted_pass, last_update, suggested_reset) VALUES
+	(1,"user1","Google","asd21fs92","2018-10-20","2018-11-20"),
+    (1,"user1-","Facebook","adq2easda","2018-09-30","2018-10-30"),
+    (1,"user1","Twitter","adfad21edasd","2018-01-01","2018-02-01"),
+    (2,"user2","USC","asd21fs92","2018-08-01","2019-08-01"),
+    (2,"user2","Google","adq2easda","2017-08-02","2018-08-02"),
+    (3,"user3","MySpace","asd21fs92","2000-01-20","2000-07-20");
+INSERT INTO security_question (passwordID, question, answer) VALUES
+    (1, "Favorite color?", "Blue"),
+    (1, "Favorite month?", "June"),
+    (1, "Favorite CS201 CP?", "Judy"),
+    (2, "Favorite color?", "Green"),
+    (3, "Name of first pet?", "Max");
+SELECT * FROM user;
+SELECT * FROM password;
+SELECT * FROM security_question;
